049fd4c34e1ab7ea03a0a57bb174605db6d852c4
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-10-19 19:48:21 +0530

    login done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,11 @@
 import 'package:biomark/components/custom_button.dart';
 import 'package:biomark/components/custom_text_field.dart';
 import 'package:biomark/pages/register_page.dart';
+import 'package:biomark/pages/profile_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,110 +17,170 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  void logInHandler() {}
+
+  void logInHandler() async {
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      // Show error message if fields are empty
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill in both fields')),
+      );
+      return;
+    }
+
+    try {
+      // Retrieve user data from Firestore
+      QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .where('account.email', isEqualTo: email)
+          .get();
+
+      if (snapshot.docs.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('User not found')),
+        );
+        return;
+      }
+
+      final userData = snapshot.docs.first.data() as Map<String, dynamic>;
+
+      // Compare the hashed password with the stored password
+      final hashedPassword = _hashData(password);
+      if (userData['account']['password'] == hashedPassword) {
+        // Successfully logged in
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Login successful')),
+        );
+        // Navigate to the next page or home page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const ProfilePage()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Incorrect password')),
+        );
+      }
+    } catch (e) {
+      print('Error logging in: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('An error occurred, please try again')),
+      );
+    }
+  }
+
   void navigateToRegister() {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => RegisterPage()),
+      MaterialPageRoute(builder: (context) => const RegisterPage()),
     );
+  }
+
+  // Function to hash data using SHA-256
+  String _hashData(String data) {
+    final bytes = utf8.encode(data);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: Center(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo
+              Icon(
+                Icons.account_circle,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+
+              // Space
+              const SizedBox(height: 25),
+
+              // Title
+              const Text(
+                "Login Here",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // Space
+              const SizedBox(height: 50),
+
+              // Email Field
+              CustomTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailController,
+              ),
+
+              // Space
+              const SizedBox(height: 10),
+
+              // Password Field
+              CustomTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: passwordController,
+              ),
+
+              // Space
+              const SizedBox(height: 10),
+
+              // Forget Password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  // logo
-                  Icon(
-                    Icons.account_circle,
-                    size: 80,
-                    color: Theme.of(context).colorScheme.inversePrimary,
-                  ),
-
-                  // space
-                  const SizedBox(height: 25),
-
-                  // Title
-                  const Text(
-                    "Login Here",
+                  Text(
+                    "Forget Password?",
                     style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.secondary,
                     ),
                   ),
-
-                  // space
-                  const SizedBox(height: 50),
-
-                  // Email Field
-                  CustomTextField(
-                    hintText: "Email",
-                    obscureText: false,
-                    controller: emailController,
-                  ),
-
-                  // space
-                  const SizedBox(height: 10),
-
-                  // Password Field
-                  CustomTextField(
-                    hintText: "Password",
-                    obscureText: true,
-                    controller: passwordController,
-                  ),
-
-                  // space
-                  const SizedBox(height: 10),
-
-                  // forget password
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Forget Password?",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // space
-                  const SizedBox(height: 25),
-
-                  // sigin in button
-                  CustomButton(text: "Log In", onTap: logInHandler),
-
-                  // space
-                  const SizedBox(height: 25),
-
-                  // don't have an account
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Don't have an account?"),
-                      TextButton(
-                          onPressed: navigateToRegister,
-                          child: Text(" Register Now",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .inversePrimary,
-                              )))
-                    ],
-                  )
                 ],
               ),
-            ),
+
+              // Space
+              const SizedBox(height: 25),
+
+              // Sign In Button
+              CustomButton(text: "Log In", onTap: logInHandler),
+
+              // Space
+              const SizedBox(height: 25),
+
+              // Don't Have an Account
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: navigateToRegister,
+                    child: Text(
+                      " Register Now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:biomark/pages/login_page.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to your profile!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            // You can add more profile details here
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Log Out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
